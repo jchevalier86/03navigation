3179d7432d5012bef79a45eb3e257a193f2437a9
Johnny Chevalier <johnny.chevalier91@gmail.com>
2024-07-04 11:47:44 +0200

    modif Task.tsx & App.css

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -9,6 +9,12 @@ const Tasks: React.FC = () => {
   const [listTasks, setListTasks] = useState<ITasks[]>([]);
   let tasks = fetchTasks;
   console.log(tasks);
+  const [modalDeleteStyle, setModalDeleteStyle] = useState("modalDeleteHidden");
+  const [idTaskToDelete, setIdTaskToDelete] = useState("");
+
+  /* --- */
+  /* Add */
+  /* --- */
 
   const addTaskInComponentTasks = async (taskToAdd: ITasks) => {
     let tasks = await addTask(taskToAdd);
@@ -21,13 +27,47 @@ const Tasks: React.FC = () => {
     getAllTasks();
   }, []);
 
-  const deleteTaskInComponentTasks = async (id: string) => {
+  /* ----- */
+  /* Modal */
+  /* ----- */
+
+  // montre le modal de suppresion
+  const deleteTaskInComponentTasks = (idRowTask: string) => {
+    //ouvrir modal de validation
+    setModalDeleteStyle("modalDeleteVisible");
+    setIdTaskToDelete(idRowTask);
+  };
+
+  // cache le modal de suppresion
+  const hideModalDelete = () => {
+    setModalDeleteStyle("modalDeleteHidden");
+    setIdTaskToDelete("");
+  };
+
+  //supprimer la lignet
+  //affiche la liste maj
+  //cache le modal
+  const validateDeleteTaskInDb = async () => {
     //ajouter une tâche
-    let tasks = await deleteTask(id);
-    console.log(tasks);
+    let task = await deleteTask(idTaskToDelete);
+    console.log(task);
     //afficher la liste
     await getAllTasks();
+
+    hideModalDelete();
   };
+
+  /* ------ */
+  /* Delete */
+  /* ------ */
+
+  // const deleteTaskInComponentTasks = async (id: string) => {
+  //   //ajouter une tâche
+  //   let tasks = await deleteTask(id);
+  //   console.log(tasks);
+  //   //afficher la liste
+  //   await getAllTasks();
+  // };
 
   //pour récupérer la liste
   const getAllTasks = async () => {
@@ -43,7 +83,21 @@ const Tasks: React.FC = () => {
     <div className="container mx-auto mt-4">
       <img src={Logo} alt="Logo Tâches" width="200" height="500"></img>
       <br />
-      <div></div>
+      <div id="supprimer" className={modalDeleteStyle}>
+        <div id="popup">
+          <div id="title">Etes-vous sûr de vouloir supprimer la tâche ?</div>
+          <button id="buttonannuler" onClick={() => hideModalDelete()}>
+            <div id="text">
+              <div id="clear">Annuler</div>
+            </div>
+          </button>
+          <button id="buttonsvalider" onClick={() => validateDeleteTaskInDb()}>
+            <div id="text2">
+              <div id="go">Valider</div>
+            </div>
+          </button>
+        </div>
+      </div>
       <div>
         <TaskForm
           addTaskInComponentTasks={(taskToAdd: ITasks) =>
